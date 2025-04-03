b0c37119544bf87c35e273c77fb6188a859eaa08
DorletaOrue <118303008+DorletaOrue@users.noreply.github.com>
2025-04-03 15:12:27 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  3 13:51:48 2025
+
+@author: N28ORIGD
+"""
+
+import pandas as pd
+import re
+import folium
+import streamlit as st
+import matplotlib.pyplot as plt
+import leafmap.foliumap as leafmap
+import os
+
+
+st.set_page_config(page_title='Dashboard',layout='wide')
+st.title('Clima Gipuzkoan')
+
+streamlit run app.py
