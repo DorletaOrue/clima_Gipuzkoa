0ef23a49f9ab29df169674fa6456cafc5bf077d4
DorletaOrue <118303008+DorletaOrue@users.noreply.github.com>
2025-04-08 15:07:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,10 +19,10 @@ st.title('Clima Gipuzkoan')
 st.markdown("""
     <style>
         [data-testid="stSidebar"] {
-            width: 800px;
+            width: 600px;
         }
         [data-testid="stSidebar"] > div:first-child {
-            width: 800px;
+            width: 600px;
         }
     </style>
 """, unsafe_allow_html=True)
