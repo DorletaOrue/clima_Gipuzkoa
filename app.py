65576d87174a4999bdb471a5d2a0450e61882a68
DorletaOrue <118303008+DorletaOrue@users.noreply.github.com>
2025-05-02 15:49:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ estaciones = Estaciones['Estación'].unique()
 estacion = st.sidebar.selectbox('Estazioa', estaciones)
 if 'selected_station' not in st.session_state:
     st.session_state['selected_station'] = None
-st_session_state['estacion']=estacion
+st.session_state['estacion']=estacion
 
 # Filter data
 filtered_data = Estaciones[(Estaciones['Estación'] == estacion) & (Estaciones['Variable'] == variable)]
